e047432f4c662363377bc848d3b14c2954f8752b
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-03-12 21:11:13 +0300

    upload image to mongodb

--- models/person.js
+++ models/person.js
@@ -4,18 +4,19 @@ const mongoose = require('../config/db');
 const Schema = mongoose.Schema; 
 //app.use(express.urlencoded());
 const personSchema = new mongoose.Schema({
-    // id:{type:String,require:true},
+    id:{type:String,require:true},
+    // default:1 
   name:{
     type:String,
-    // required:true
+    required:true
   },
   portfileText:{
     type:String,
-    // required:true
+    required:true
   },
   portfileImage:{
     type:String,
-    // required:true
+    required:true
   }});
 const person = mongoose.model("persons",personSchema);
 module.exports = person;
--- routes/portodash.js
+++ routes/portodash.js
@@ -1,10 +1,45 @@
 var express = require('express');
+const { default: mongoose } = require('mongoose');
 var router = express.Router();
+const multer = require('multer');
+const app = require('../app');
+const storage = multer.diskStorage({
+  destination:(req,file,cb)=>{
+    if(file.mimetype=="image/png" || file.mimetype=="image/jpg")
+    cb(null,"public/uploads/")
+    else if(file.mimetype== "application/pdf")
+    cb(null,"public/pdf/")
+  },filename:(req,file,cb)=>{
+    var extension = file.originalname.split(".");
+    var ext = extension[extension.length - 1];
+
+    //var uploaded_file_name =Date.now() + '-' + Math.round(Math.random() * 1E9)+file.originalname;
+    var uploaded_file_name =
+      file.fieldname +
+      "-" +
+      Date.now() +
+      "-" +
+      Math.round(Math.random() * 1e9) +
+      "." +
+      ext;
+
+    cb(null, uploaded_file_name);
+  }
+});
+const upload = multer({ 
+  storage:storage,
+fileFilter:(req,file,callback)=>{
+ if(file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "application/pdf"){
+   callback(null,true)
+ }
+ else callback(null,false)
+  // file.mimetype
+  // callback()
+},  limits:1024*1024 * 5
+});
 const Person = require('../models/person');
 
 /* GET users listing. */
-
-
 //--------------------------------
 ///protofile
 router.get('/protofile',auth,(req,res)=>{
@@ -14,12 +49,15 @@ router.get('/protofile',auth,(req,res)=>{
 router.get("/add_person", auth, (req, res) => {
   res.render("protofile");
 });
-router.post('/add_person',auth,(req,res)=>{
+router.post('/add_person',upload.single('pro_image'),(req,res)=>{
     const p = new Person(
       {
+          id:mongoose.Types.ObjectId,
           name: req.body.namep,
           portfileText:req.body.profile_desc,
-          portfileImage:req.body.pro_image
+          // portfileImage:req.body.pro_image
+          portfileImage:req.file.filename
+          
       }   
     )
     p.save((error,result)=>{
